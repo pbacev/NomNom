5f92ec71f67f26ab1d4e23f9e801b1fa2111ba08
pbacev <pbacev@gmail.com>
2025-05-30 10:08:25 +0200

    posledna smena nz za sho

--- app/src/main/java/com/example/nomnom/MainActivity.kt
+++ app/src/main/java/com/example/nomnom/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
         FirebaseApp.initializeApp(this)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
+            //.requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
